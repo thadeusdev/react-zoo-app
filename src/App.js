f1053fd02483db31b6b34fdd64ada50b3df190cf
thadeus <thadeusnyariki94@gmail.com>
2022-09-29 23:45:02 +0300

    Ongoing with form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Animals from './Animals';
+import Form from './Form'
 
 function App() {
   return (
     <div>
       <h1>Welcome to my zoo</h1>
+      <Form />
       <Animals />
     </div>
   );
---
+++ src/Form.js
@@ -0,0 +1,14 @@
+import react from 'react'
+
+function Form(){
+    return (
+        <>
+            <form>
+                <label >Name: </label>
+                <input type='text' placeholder="Enter name" />
+            </form>
+        </>
+    )
+}
+
+export default Form
